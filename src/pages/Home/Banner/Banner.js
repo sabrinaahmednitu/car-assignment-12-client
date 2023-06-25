915933039c6048b32f779bc77cc2740a9a168bbf
sabrina <sabrinaahmednitu@gmail.com>
2023-06-25 13:00:08 +0600

    banner complete

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -5,10 +5,10 @@ const Banner = () => {
     return (
       <div className="banner">
         <div className=" banner-text  ">
-          <h1 className="text-light text-start">
+          <h1 className=" text-start">
             CarHouse Brand New <br /> Car Collection - 2023 Edition
           </h1>
-          <p className="text-light text-start">
+          <p className=" text-start">
             A car dealership, or vehicle local distribution, is a business that
             sells new or used cars at the retail level, <br /> based on a
             dealership contract with an automaker or its sales subsidiary.
